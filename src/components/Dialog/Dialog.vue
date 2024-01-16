58a80a697a69f57c0c0d343a7dd30b43819f60cc
freedomwebcn <freedomweb@yeah.net>
2024-01-16 14:01:39 +0800

    整理代码

--- src/components/Dialog/Dialog.vue
+++ src/components/Dialog/Dialog.vue
@@ -1,7 +1,5 @@
 <template>
-  <Transition>
-    <div class="fixed left-[50%] top-[50%] z-50 -translate-x-1/2 -translate-y-1/2" @click.stop>
-      <slot></slot>
-    </div>
-  </Transition>
+  <div class="fixed left-[50%] top-[50%] z-50 -translate-x-1/2 -translate-y-1/2" @click.stop>
+    <slot></slot>
+  </div>
 </template>
